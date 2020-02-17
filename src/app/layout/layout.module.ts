d2b3fe3aed736c557e4830e01b729a5c0870efa5
balsalobre <carlosocb@gmail.com>
2020-02-17 20:16:06 +0100

    Initial commit

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent,
+    LayoutComponent,
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class LayoutModule { }
